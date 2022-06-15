4c383f720ec8afb68d03c8a42c3cc649c14249de
alexmaga17 <alexmagalhaes17@gmail.com>
2022-06-15 17:01:49 +0100

    cors

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT;
 //     origin: "http://localhost:8080"
 // };
 
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 // root route -- /api/
 app.get('/', function (req, res) {
